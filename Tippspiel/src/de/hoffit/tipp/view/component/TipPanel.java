a10ce4f154124697b52606516499d2415427b40b
Tony <hoffmanntony@gmx.de>
2012-06-06 23:13:51 +0200

    Test

--- Tippspiel/src/de/hoffit/tipp/view/component/TipPanel.java
+++ Tippspiel/src/de/hoffit/tipp/view/component/TipPanel.java
@@ -26,7 +26,6 @@ import javax.swing.border.BevelBorder;
 public class TipPanel extends JPanel {
 
 	public static final String DAY_CHOICE = "dayChoice";
-
 	// Test
 	private static final long serialVersionUID = 8488504806410446897L;
 
